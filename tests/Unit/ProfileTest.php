aaa75ba2fdcdb1a0a1e84c70a7765adb542d5566
rajerr <rajerr2013@gmail.com>
2020-11-25 09:51:37 +0000

    --- Auto-Git Commit ---

--- tests/Unit/ProfileTest.php
+++ tests/Unit/ProfileTest.php
@@ -8,9 +8,22 @@ class ProfileTest extends TestCase
 {
     private Profile $profile;
 
-    protected function testProfile()
+    protected function setProfile():void
     {
-        parent::testProfile();
+        parent::setProfile();
+
         $this->profile = new Profile();
+    }
+
+    public function testGetLibelle():void
+    {
+        $value ="Myprofile";
+        
+        $response = $this->profile->setLibelle($value);
+
+        self::assertInstanceOf(Profile::class, $response);
+
+        self::assertEquals($value, $this->profile->getLIbelle());
+        self::assertEquals($value, $this->profile->getStatut());
     }
 }
