aa0740a2e3348e9b6006d8939a195f1d27b633b7
rajerr <rajerr2013@gmail.com>
2020-11-25 12:22:07 +0000

    --- Auto-Git Commit ---

--- src/DataFixtures/CompetenceFixtures.php
+++ src/DataFixtures/CompetenceFixtures.php
@@ -3,6 +3,8 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
+use App\Entity\Competence;
+use App\Entity\NiveauEvaluation;
 use Doctrine\Persistence\ObjectManager;
 use App\DataFixtures\CompetenceFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -28,20 +30,21 @@ class CompetenceFixtures extends Fixture
         $tab = ['Niveau 1','Niveau 2','Niveau 3'];
 
         $competence = new Competence();
-            $competence->setLibelle($faker->libelle);
-            $user->setDescriptif($faker->comment);
-            $user->setStatut(true);
+            $competence->setLibelle($faker->sentence);
+            $competence->setDescriptif($faker->text);
+            $competence->setStatut(true);
             $manager->persist($competence);
 
-            for ($i=1; $i <= count($tab) ; $i++) { 
+            for($i=0; $i < count($tab) ; $i++)
+            { 
                 $niveau= new NiveauEvaluation();
                 $niveau->setLibelle($tab[$i])
-                       ->setCritereEvaluaton($tab_compet["niveauEvaluations"][$i]["critereEvaluation"])
-                       ->setGroupeAction($tab_compet["niveauEvaluations"][$i]["critereEvaluation"]);
+                       ->setCritereEvaluaton($faker->text)
+                       ->setGroupeAction($faker->text);
                 $manager->persist($niveau);
-                $new_compet->addNiveauEvaluation($niveau);
             }
-            $manager->persist($new_compet);
+            $manager->persist($competence);
+
         $manager->flush();
     }
 }
