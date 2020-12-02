3dfc729d07e8f36fdac4b778ecd0c69f244d3877
rajerr <rajerr2013@gmail.com>
2020-12-02 23:34:55 +0000

    --- Auto-Git Commit ---

--- src/DataFixtures/PromoFixtures.php
+++ src/DataFixtures/PromoFixtures.php
@@ -3,7 +3,8 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
-use App\Entity\GroupeCompetence;
+use App\Entity\Promo;
+use App\Entity\Groupe;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 
@@ -16,24 +17,22 @@ class PromoFixtures extends Fixture
         for($i= 0; $i < 3; $i++){
             $promo = new Promo();
             
-            $promo->setLibelle("Promo".[$i]);
-            $promo->setLangue($faker->language);
-            $promo->setLangue($faker->title);
+            $promo->setLangue(('french'));
+            $promo->setTitre($faker->title);
             $promo->setdescription($faker->text);
             $promo->setLieu($faker->country);
             $promo->setReferenceAgate($faker->sentence);
             $promo->setChoixFabrique("Sonatel Academy");
-            $promo->setDateDebut($faker->date);
-            $promo->setDateFin($faker->date);
+            $promo->setDateDebut($faker->datetime);
+            $promo->setDateFin($faker->datetime);
             
             $groupe = new Groupe();
 
-                $groupe->setNom();
+                $groupe->setNom("Groupe_Principal");
                 $groupe->setDateCreation($faker->date);
+                $manager->persist($groupe);
 
-                $groupe->setNom();
-                $groupe->setNom();
-                 $manager->persist($groupeRef);
+            $promo->addGroupe($groupe);
 
             $manager->persist($promo);
         }
