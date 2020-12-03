5e67df977ae43ff13b554a35193b34465b63280c
rajerr <rajerr2013@gmail.com>
2020-12-03 14:27:37 +0000

    --- Auto-Git Commit ---

--- src/Controller/PromoController.php
+++ src/Controller/PromoController.php
@@ -2,19 +2,39 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class PromoController extends AbstractController
 {
     /**
-     * @Route("/promo", name="promo")
+     * @Route("api/admin/promo", name="add_promo"),
+     * methods={"POST"}
      */
-    public function index(): Response
+    public function addPromo(): Response
     {
-        return $this->render('promo/index.html.twig', [
-            'controller_name' => 'PromoController',
-        ]);
+        $promo = $request->request->all();
+        dd($promo);
+        $avatar = $request->files->get("avatar");
+        $avatar = fopen($avatar->getRealPath(), "rb");
+        $promo["avatar"] = $avatar;
+        $referentiel = $promo['referentiel'];
+            $promo = $serializer->denormalize($promo, "App\Entity\Referentiel");
+        $errors = $validator->validate($promo);
+        if ($errors){
+            $errors = $serializer->serialize($errors, "json");
+            return new JsonResponse($errors, Response::HTTP_BAD_REQUEST, [], true);
+        }
+        $promo->setDateDebut(new \DateTime('now'));
+        dd($promo);
+        $manager->persist($promo);
+        $manager->flush();
+
+        return  $this->json($promo, Response::HTTP_CREATED);
+
+        fclose($avatar);
     }
+
 }
